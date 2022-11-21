e4696541eeb48afff3a7f066d4ef94a90ed9adf8
Mateus Jackson <mateusjkson@gmail.com>
2022-11-21 12:59:54 -0300

    First controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,14 +17,15 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your Contact Page.";
 
             return View();
         }
